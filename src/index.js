3e2b60ee56164da5b7b7f9892b9232547aa61cf4
clement senou <clementsenou74@gmail.com>
2023-02-03 18:32:01 -0600

    complet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.getElementById("searchByID")
+    // input.textContent = "text"
+    // inputForm.append(input)
+    console.log(input.value);
+    fetch('http://localhost:3000/movies')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
